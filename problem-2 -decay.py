3151e3e773fa5b77a2645dc78c9ca86c64700cbd
ethanlawhead <ethanlawhead@hotmail.com>
2024-01-10 17:21:04 -0700

    formatted plot titles

--- problem-2 -decay.py
+++ problem-2 -decay.py
@@ -1,4 +1,13 @@
-import matplotlib.ppylot as ppylot
+import matplotlib.pyplot as plt
+import numpy as np
 
-fig, ax = plt.subplots()
+
+#variables
+no = 1000
+
+plt.plot([1,2,3,4])
+plt.title('Radioactive Decay with Varying λ')
+plt.xlabel("Time (s)")
+plt.ylabel('Number of Radioactive Atoms')
+plt.show()
 
