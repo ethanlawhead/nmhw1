aa5fcaf790a0203cd4455fee21f9db57d9a83961
ethanlawhead <ethanlawhead@hotmail.com>
2024-01-10 17:37:06 -0700

    started plot stuff

--- problem-2 -decay.py
+++ problem-2 -decay.py
@@ -1,13 +1,25 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import math
 
 
 #variables
 no = 1000
+lams = [0.01,0.1,1,10]
 
-plt.plot([1,2,3,4])
+plt.plot(no*math.e**(-0.01))
+
+
+
+
+
+#plot formatting
+
 plt.title('Radioactive Decay with Varying λ')
 plt.xlabel("Time (s)")
 plt.ylabel('Number of Radioactive Atoms')
+plt.legend()
+#show plot
 plt.show()
 
+#save plot
