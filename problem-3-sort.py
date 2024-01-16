b756570f02904013603b3a6fbd48e5134b02509f
ethanlawhead <ethanlawhead@hotmail.com>
2024-01-16 09:54:20 -0700

    added sorting and mean

--- problem-3-sort.py
+++ problem-3-sort.py
@@ -2,23 +2,26 @@
 user = input('what are 10 numbers, separate with comma')
 raw = user.split(',')
 num = []
-
+new = []
+sum = 0
 #putting input into integer list
 for i in raw:
     num.append(i)
 
-lowest = num[0]
 
+#sorting
+for i in range(0, len(num)):
+    for j in range(i +1 , len(num)):
+        if num[i] >= num[j]:
+            num[i], num[j] = num[j] , num[i]
+        
+#mean
 for i in num:
-    print(i)
-    # if int(num[i]) < int(lowest):
-    #     lowest = num[i]
-    # for j in num:
-    #     if num[j] < num[i]:
-    #         num_1 = num[j]
-    #         num_2 = num[i]
+    sum = sum + int(i)
+mean = sum/(len(num))
 
-    #         num[i] = num_1
-    #         num[j] = num_2
-print(num)
-print(lowest)
+
+
+
+print("the sorted list is", num)
+print ("The mean is" , mean)
