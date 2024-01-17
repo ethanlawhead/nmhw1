43315a72056a48936c9fec8ef0ca502b88e2d030
ethanlawhead <ethanlawhead@hotmail.com>
2024-01-17 12:55:21 -0700

    graph is completed

--- problem-2 -decay.py
+++ problem-2 -decay.py
@@ -9,7 +9,7 @@ time = np.linspace(0,60,50)
 lams = [0.1,0.35,0.75,1]
 for i in lams:
     numbers = no*math.e**(-i*time)
-    plt.plot(time,numbers)
+    plt.plot(time,numbers, label = 'λ = '+ str(i))
 
 
 
@@ -21,6 +21,7 @@ plt.title('Radioactive Decay with Varying λ')
 plt.xlabel("Time (s)")
 plt.ylabel('Number of Radioactive Atoms')
 plt.legend()
+
 #show plot
 plt.show()
 
