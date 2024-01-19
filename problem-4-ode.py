2d38916491e215eb3634464a6c953d29f64b575e
ethanlawhead <ethanlawhead@hotmail.com>
2024-01-19 15:38:47 -0700

    added comments, variables for d

--- problem-4-ode.py
+++ problem-4-ode.py
@@ -5,16 +5,19 @@ import matplotlib.pyplot as plt
 #initial variables
 g = 10
 m1= 10
+m2 = 100
 c = 50
 delt = 0.1
 
 time = np.linspace(0,2,100)
 time2 = np.arange(0,2.1,delt)
+
 #function for terminal velocity
 def vt(gravity,mass,drag):
     vt = int((gravity*mass)/drag)
     return vt
 
+#function for eq 3
 def eq3(vi,g,c,m,delt,t):
     eqlst = []
     for i in t:
@@ -28,6 +31,7 @@ def eq3(vi,g,c,m,delt,t):
 
     return eqlst
 
+#function to find time for 99.99% vt
 def percent(terminal, eq2, eq3):
    print('not done yet')
    
@@ -35,10 +39,20 @@ def percent(terminal, eq2, eq3):
 #part a
 va = vt(g,m1,c)
 
-y_valeq3 = eq3(0,g,c,m1,delt,time2)
 
 v2 = va*(1-math.e**(-(c/m1)*time))
 
+#part b
+y_valeq3 = eq3(0,g,c,m1,delt,time2)
+
+#part c
+
+
+#part d
+y_vald = eq3(0,g,c,m2,delt,time2)
+print(y_vald)
+
+#plotting
 plt.plot(time,v2, label = 'part a')
 plt.plot(time2,y_valeq3, label = 'part b')
 
