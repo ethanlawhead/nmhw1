467afd8a4493bfae0e912c160c7b78ffc33c1a20
ethanlawhead <ethanlawhead@hotmail.com>
2024-01-19 15:32:17 -0700

    added plot labels, cleaned code, started percent of vt function

--- problem-4-ode.py
+++ problem-4-ode.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 #initial variables
 g = 10
-m= 10
+m1= 10
 c = 50
 delt = 0.1
 
@@ -12,7 +12,7 @@ time = np.linspace(0,2,100)
 time2 = np.arange(0,2.1,delt)
 #function for terminal velocity
 def vt(gravity,mass,drag):
-    vt = int((g*m)/c)
+    vt = int((gravity*mass)/drag)
     return vt
 
 def eq3(vi,g,c,m,delt,t):
@@ -27,22 +27,20 @@ def eq3(vi,g,c,m,delt,t):
        vi = v
 
     return eqlst
-# def eq3(vi,g,c,m,t,delt):
-#     for i in t:
-#         v = vi+(g-(c/m)*vi)*delt
-#         vi = v
-#     plt.plot(t,v)
-#     return v
+
+def percent(terminal, eq2, eq3):
+   print('not done yet')
+   
 
 #part a
-va = vt(g,m,c)
+va = vt(g,m1,c)
 
-y_valeq3 = eq3(0,g,c,m,delt,time2)
+y_valeq3 = eq3(0,g,c,m1,delt,time2)
 
-v2 = va*(1-math.e**(-(c/m)*time))
-print(1-math.e**(-(c/m)*time))
-plt.plot(time,v2)
-plt.plot(time2,y_valeq3)
+v2 = va*(1-math.e**(-(c/m1)*time))
+
+plt.plot(time,v2, label = 'part a')
+plt.plot(time2,y_valeq3, label = 'part b')
 
 
 
