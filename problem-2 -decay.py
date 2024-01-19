0637b6a1ebb044208103bd9b1f6eb7d99c836f71
ethanlawhead <ethanlawhead@hotmail.com>
2024-01-19 11:42:28 -0700

    finished problem 2

--- problem-2 -decay.py
+++ problem-2 -decay.py
@@ -5,13 +5,13 @@ import math
 
 #variables
 no = 500
-time = np.linspace(0,60,50)
+time = np.linspace(0,20,50)
 lams = [0.1,0.35,0.75,1]
 for i in lams:
     numbers = no*math.e**(-i*time)
     plt.plot(time,numbers, label = 'λ = '+ str(i))
 
-
+    plt.text(time[-1]/8,(no*np.exp(-i*(time[-1])/8)),'λ = '+ str(i))
 
 
 
@@ -22,7 +22,8 @@ plt.xlabel("Time (s)")
 plt.ylabel('Number of Radioactive Atoms')
 plt.legend()
 
+#save plot
+plt.savefig('decay.png')
+
 #show plot
 plt.show()
-
-#save plot
