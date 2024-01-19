b0ad6073c2d040c234939b428a80b8c1c7e5ac76
ethanlawhead <ethanlawhead@hotmail.com>
2024-01-19 12:30:58 -0700

    added vt function

--- problem-4-ode.py
+++ problem-4-ode.py
@@ -2,20 +2,27 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 
+#initial variables
 g = 10
 m= 10
 c = 50
 time = np.linspace(0,2,1000)
 
-vt = int((g*m)/c)
-v2 = vt*(1-math.e**(-(c/m)**time))
+#function for terminal velocity
+def vt(gravity,mass,drag):
+    vt = int((g*m)/c)
+    return vt
+#part a
+va = vt(g,m,c)
+
+v2 = va*(1-math.e**(-(c/m)**time))
 plt.plot(time,v2)
 
-print(v2)
-
+#plot formatting
 plt.title('Velocity of a Falling Body')
 plt.xlabel("Time (s)")
 plt.ylabel('Velocity (m/s)')
 plt.legend()
 
+#show plot
 plt.show()
